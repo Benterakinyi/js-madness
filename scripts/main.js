a392fd2baf4cea58ca19384642ad3400dbfa705e
tumaa <ramanfatuu@gmail.com>
2019-09-02 23:03:45 +0300

    declare variables and give them a key value

---
+++ scripts/main.js
@@ -0,0 +1,79 @@
+var Next ={
+fact :"A light-weight framework for static and server rendering applications."
+}
+var Numeric ={
+fact:"Numerical analysis in Javascript"
+}
+
+var Offline ={
+fact:"Automatically display online/offline indication to your users."
+}
+var Ocanvas ={
+fact:"JavaScript library for object-based canvas drawing."
+}
+var Polmer ={
+fact:"Leverage the future of the web platform today."
+}
+var Pace ={
+fact:"Automatic add progress bar to the site."
+}
+var Q ={
+fact:"A tool for making and composing asynchronous promises in JavaScript."
+}
+var Qtip2 ={
+fact:"Pretty powerful tooltips"
+}
+var React ={
+fact:"A library for building user interface"
+}
+var Relay ={
+fact:"Relay is a JavaScript framework for building data-driven React applications."
+}
+var String ={
+fact: "Extra JavaScript string methods. "
+}
+var Spin ={
+fact: "A spinning activity indicator,"
+}
+var Typed ={
+fact:"Helps you create a typing animation on your web page."
+}
+var Tether ={
+fact: "A posioning engine to make overlay and dropdown better"
+}
+var Uppy ={
+fact:"The next open source file uploader for web browsers."
+}
+var UaParser ={
+fact:"Lightweight JavaScript-based User-Agent string parser."
+}
+var Video ={
+fact:"Universal video embed."
+}
+var Vide={
+fact: "Easy as hell jQuery plugin for video backgrounds."
+}
+var Wpaint={
+fact:"jquery paint plugin."
+}
+var Xeditable={
+fact:"This library allows you to create editable elements on your page."
+}
+var Yarn ={
+fact:"Fast, reliable, and secure dependency management."
+}
+var Yeoman={
+fact:"a set of tools for automating development workflow."
+}
+var Zam ={
+fact:"⚡️ A component based library that encourages single-page applications."
+}
+var Zip ={
+fact:"JavaScript library to zip and unzip files."
+}
+// var nude ={
+
+// }
+// var nude ={
+
+// }
