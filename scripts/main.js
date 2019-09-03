444031c5a31a39d3e4794642b8cc26ca807a2f4b
mutembeijoe <mutembeijoe@gmail.com>
2019-09-03 15:21:18 +0300

    intergrated timer and random letter function to output to the dom

--- scripts/main.js
+++ scripts/main.js
@@ -194,15 +194,15 @@ var limit = alphabetArray.length-1;
 function startGame(){
     const letter = generateRandomLetter().toLowerCase();
     console.log(`letter: ${letter}`)
+   
     for (let i=0;i<=limit;i++) {
         (function(ind) {
          
             setTimeout(function(){
                 console.log(alphabetArray[i]);
-     
+                $('#unselected').text(`${alphabetArray[i]}`)
                 if(alphabetArray[ind] == letter){
-                    // The letter we want
-                    console.log(letter);
+                    $('#random-generator').text(`${letter}`)
                      secondsTimer();
                      
                  }
@@ -219,9 +219,9 @@ function secondsTimer(){
     let time = 15;
     const myInterval = setInterval(function(){
         let sec = time --;
-        console.log(sec)
+        $('#timer').text(sec);
         if(sec <= 1){
-            console.log("stopped")
+            $('#timer').text("Time's out")
             clearInterval(myInterval);
         }
 
@@ -231,6 +231,10 @@ function secondsTimer(){
 //  UI LOGIC
 $(document).ready(function(){
     console.log('Hello world, jQuery works!')
+
+    $('#playButton').click(function(){
+        startGame();
+    })
 })
 
 
