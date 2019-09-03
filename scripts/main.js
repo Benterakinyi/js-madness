13d465298dcf7fa9b968b0b7dfea3460bff917a6
mutembeijoe <mutembeijoe@gmail.com>
2019-09-03 13:34:50 +0300

    changed the playButton identification from a class to an id in the css and html

--- scripts/main.js
+++ scripts/main.js
@@ -185,14 +185,15 @@ function generateRandomLetter(){
     return String.fromCharCode(65 + number);
 }
 
-const letter = generateRandomLetter().toLowerCase();
-console.log(`letter: ${letter}`)
+
 const alphabetString = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z";
 const alphabetArray = alphabetString.split(',');
 
 var limit = alphabetArray.length-1;
 
 function startGame(){
+    const letter = generateRandomLetter().toLowerCase();
+    console.log(`letter: ${letter}`)
     for (let i=0;i<=limit;i++) {
         (function(ind) {
          
