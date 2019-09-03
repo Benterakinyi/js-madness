e92f200560dd37d9d25536acde614dc768d90242
tumaa <ramanfatuu@gmail.com>
2019-09-03 11:03:01 +0300

    this will create array of javascript starting from N to z .

--- scripts/main.js
+++ scripts/main.js
@@ -1,79 +1,183 @@
-var Next ={
-fact :"A light-weight framework for static and server rendering applications."
-}
-var Numeric ={
-fact:"Numerical analysis in Javascript"
-}
 
-var Offline ={
-fact:"Automatically display online/offline indication to your users."
-}
-var Ocanvas ={
-fact:"JavaScript library for object-based canvas drawing."
-}
-var Polmer ={
-fact:"Leverage the future of the web platform today."
-}
-var Pace ={
-fact:"Automatic add progress bar to the site."
-}
-var Q ={
-fact:"A tool for making and composing asynchronous promises in JavaScript."
-}
-var Qtip2 ={
-fact:"Pretty powerful tooltips"
-}
-var React ={
-fact:"A library for building user interface"
-}
-var Relay ={
-fact:"Relay is a JavaScript framework for building data-driven React applications."
-}
-var String ={
-fact: "Extra JavaScript string methods. "
-}
-var Spin ={
-fact: "A spinning activity indicator,"
-}
-var Typed ={
-fact:"Helps you create a typing animation on your web page."
-}
-var Tether ={
-fact: "A posioning engine to make overlay and dropdown better"
-}
-var Uppy ={
-fact:"The next open source file uploader for web browsers."
-}
-var UaParser ={
-fact:"Lightweight JavaScript-based User-Agent string parser."
-}
-var Video ={
-fact:"Universal video embed."
-}
-var Vide={
-fact: "Easy as hell jQuery plugin for video backgrounds."
-}
-var Wpaint={
-fact:"jquery paint plugin."
-}
-var Xeditable={
-fact:"This library allows you to create editable elements on your page."
-}
-var Yarn ={
-fact:"Fast, reliable, and secure dependency management."
-}
-var Yeoman={
-fact:"a set of tools for automating development workflow."
-}
-var Zam ={
-fact:"⚡️ A component based library that encourages single-page applications."
-}
-var Zip ={
-fact:"JavaScript library to zip and unzip files."
-}
-// var nude ={
 
-// }
-// var nude ={
+const librayarray = [
+    {
+        name: "next",
+        fact: " is used for light-weight framework for static and server rendering applications,"
 
-// }
+    },
+    {
+        name: "numeric",
+        fact: "is  used for Numerical analysis in Javascript",
+    },
+
+    {
+        name: "offline",
+        fact: " used to automatically display online/offline indication to your users,"
+    },
+
+    {
+        name: " ocanvas",
+        fact: "this used for JavaScript library for object-based canvas drawing",
+
+    },
+
+    {
+        name: "polmer",
+        fact: " this would be the Leverage the future of the web platform today",
+
+    },
+
+
+    {
+        name: "pace",
+        fact: "Automatic add progress bar to the site",
+    },
+
+    {
+        name: "q",
+        fact: "A tool for making and composing asynchronous promises in JavaScript."
+
+    },
+
+    {
+        name: "qtip2",
+        fact: " this is Pretty powerful tooltips",
+    },
+
+
+    {
+        name: "react",
+        fact: " is javascript library for building user interface",
+
+    },
+
+    {
+        name: "relay",
+        fact: "Relay is a javaScript framework for building data-driven React applications."
+
+    },
+
+    {
+        name: "string",
+        fact: " this is used for extra JavaScript string methods",
+    },
+
+    {
+        name: "spin",
+        fact: "this is spinning activity indicator",
+
+    },
+
+    {
+        name: "typed",
+        fact: " this helps you create a typing animation on your web page",
+
+    },
+    {
+        name: "tether",
+        fact: "A posioning engine to make overlay and dropdown better",
+
+    },
+
+    {
+        name: "uppy",
+        fact: "The next open source file uploader for web browsers",
+
+    },
+
+    {
+        name: "uaParser",
+        fact: "Lightweight JavaScript-based User-Agent string parser,"
+    },
+    {
+        name: "video",
+        fact: "Universal video embed",
+
+    },
+
+    {
+        name: "vide",
+        fact: "Easy as helname",
+    },
+    {
+        name: "video",
+        fact: " this used for universal video embed",
+    },
+
+    {
+        name: "wpaint",
+        fact: " this is jquery paint plugin",
+    },
+
+    {
+        name: "xeditable",
+        fact: "This library allows you to create editable elements on your page",
+    },
+
+    {
+        name: "yarn",
+        fact: " this is used for fast, reliable, and secure dependency management",
+
+    },
+
+    {
+        name: "yeoman",
+        fact: "its good for set of tools for automating development workflow",
+
+    },
+
+    {
+        name: "zip",
+        fact: "this is javaScript library to zip and unzip files",
+    },
+
+    {
+        name: "react native",
+        fact: " this is  framework for building native apps with React",
+
+    },
+
+
+    {
+        name: "bootstrap vue",
+        fact: "BootstrapVue provides one of the most comprehensive implementations of Bootstrap 4 components and grid system for Vue.js and with extensive and automated WAI-ARIA accessibility markup",
+
+    },
+
+
+    {
+        name:"zepto",
+        fact:"Zepto is a minimalist JavaScript library for modern browsers with a largely jQuery-compatible API. If you use jQuery, you already know how to use Zepto",
+        
+    },
+
+    {
+        name:"rxjs",
+        fact:" this is the Reactive Extensions for JavaScript",
+        
+    },
+
+    {
+        name:"openlayers",
+        fact:"OpenLayers makes it easy to put a dynamic map in any web page. It can display map tiles, vector data and markers loaded from any source",
+    },
+
+    {
+        name:"webpack",
+        fact:"webpack is a module bundler. Its main purpose is to bundle JavaScript files for usage in a browser",
+
+        
+    },
+
+    
+    {
+        name:"Perfect Scrollbar",
+        
+        fact:" its tiny but perfect jQuery scrollbar plugin", 
+    },
+
+    ]
+
+
+
