08064e8ef8b3095054b5823c7a8b0f21c624a37e
mutembeijoe <mutembeijoe@gmail.com>
2019-09-03 07:20:04 +0300

    create a function that loops through all alhabetic letters with a setTimeOut until it mathes the generated random letter

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,31 @@
 function generateRandomLetter(){
     const number = Math.floor(Math.random()*Math.floor(26));
     return String.fromCharCode(65 + number);
-    
 }
 
-const letter = generateRandomLetter();
+const letter = generateRandomLetter().toLowerCase();
+console.log(`letter: ${letter}`)
+const alphabetString = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z";
+const alphabetArray = alphabetString.split(',');
+
+var limit = alphabetArray.length-1;
+
+function startGame(){
+    for (let i=0;i<=limit;i++) {
+        (function(ind) {
+         
+            setTimeout(function(){
+                console.log(alphabetArray[i]);
+     
+                if(alphabetArray[ind] == letter){
+                    // The letter we want
+                     console.log(letter);
+                 }
+            }, 100 + (500 * ind));
+        })(i);
+     
+     }
+     
+}
+
+// startGame();
