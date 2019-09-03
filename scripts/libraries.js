a1730841b35d9c5eb35b0a9b8e4005b67956a06f
daydroid <daydroidmuchiri@gmail.com>
2019-09-03 11:07:04 +0300

    created array of libraries from a to m

---
+++ scripts/libraries.js
@@ -0,0 +1,175 @@
+const libraryArray = [{
+        name: "Mustache",
+        fact: "mustache.js is an implementation of the mustache template system in JavaScript.",
+    },
+    {
+        name: "Loopback",
+        fact: " is a highly-extensLoopBackible, open-source Node.js framework that enables you to Create dynamic end-to-end REST APIs with little or no coding.",
+
+    }, {
+        name: "Howler.js",
+        fact: "It defaults to Web Audio API and falls back to HTML5 Audio.",
+    }, {
+        name: "Cypress",
+        fact: "used for front-end web testing",
+    }, {
+        name: "Knex",
+        fact: "Knex.js is a batteries included SQL query builder for Postgres, MSSQL, MySQL, MariaDB, SQLite3, Oracle, and Amazon Redshift designed to be flexible, portable, and fun to use.",
+    }, {
+        name: "Jade",
+        fact: "Pug is a high-performance template engine heavily influenced by Haml and implemented with JavaScript for Node.js and browsers",
+    }, {
+        name: "Impress.js",
+        fact: "It's a presentation framework based on the power of CSS3 transforms and transitions in modern browsers and inspired by the idea behind prezi.com.",
+    }, {
+        name: "Gulp",
+        fact: "gulp is a toolkit that helps you automate painful or time-consuming tasks in your development workflow."
+    }, {
+        name: "Jasmine",
+        fact: "Jasmine is a Behavior Driven Development testing framework for JavaScript. It does not rely on browsers, DOM, or any JavaScript framework.",
+    }, {
+        name: "MobX",
+        fact: "Generally speaking any function can become a reactive view that observes its data, and MobX can be applied in any ES5 conformant JavaScript environment.",
+    }, {
+        name: "Koa",
+        fact: "Koa's middleware stack flows in a stack-like manner, allowing you to perform actions downstream then filter and manipulate the response upstream",
+    }, {
+        name: "Highlight.js",
+        fact: "Highlight.js is a syntax highlighter written in JavaScript.",
+    }, {
+        name: "fullPage.js",
+        fact: "It allows the creation of fullscreen scrolling websites, as well as adding some landscape sliders inside the sections of the site.",
+    }, {
+        name: "Modernizr",
+        fact: "Modernizr tests which native CSS3 and HTML5 features are available in the current UA and makes the results available to you in two ways: as properties on a global Modernizr object, and as classes on the <html> element.",
+    }, {
+        name: "immutable.js",
+        fact: "It provides many Persistent Immutable data structures",
+    }, {
+        name: "draft.js",
+        fact: "Draft.js is used in production on Facebook, including status and comment inputs, Notes, and messenger.com",
+    }, {
+        name: "blueprint",
+        fact: "It is optimized for building complex, data-dense web interfaces for desktop applications which run in modern browsers and IE11.",
+    }, {
+        name: "day.js",
+        fact: "Day.js is a minimalist JavaScript library that parses, validates, manipulates, and displays dates and times for modern browsers with a largely Moment.js-compatible API",
+    }, {
+        name: "jQuery",
+        fact: "jQuery is a JavaScript library designed to simplify HTML DOM tree traversal and manipulation, as well as event handling, CSS animation, and Ajax",
+    }, {
+        name: "Echarts ",
+        fact: "ECharts is a free, powerful charting and visualization library offering an easy way of adding intuitive, interactive, and highly customizable charts to your commercial products",
+    }, {
+        name: "codemirror",
+        fact: "It is specialized for editing code, and comes with over 100 language modes and various addons that implement more advanced editing functionality",
+    }, {
+        name: "Lodash",
+        fact: "Lodash is a JavaScript library which provides utility functions for common programming tasks using the functional programming paradigm",
+    }, {
+        name: "Ember",
+        fact: "It allows developers to create scalable single-page web applications by incorporating common idioms and best practices into the framework.",
+    }, {
+        name: "Foundation",
+        fact: "Foundation is a responsive front-end framework. Foundation provides a responsive grid and HTML and CSS UI components, templates, and code snippets, including typography, forms, buttons, navigation and other interface elements, as well as optional functionality provided by JavaScript extensions",
+    }, {
+        name: "leaflet",
+        fact: "Leaflet is a widely used open source JavaScript library used to build web mapping applications.",
+    }, {
+        name: "Chart.js",
+        fact: "Chart.js is an easy way to include animated, interactive graphs on your website for free",
+    }, {
+        name: "Electron",
+        fact: "Electron allows for the development of desktop GUI applications using web technologies",
+    }, {
+        name: "Babel",
+        fact: "When your supportedAntDesign environments don't support certain features natively, Babel will help you compile those features down to a supported version",
+    }, {
+        name: "AntDesign",
+        fact: "Ant Design, also known as “Antd” is a components library for ReactJS.",
+    }, {
+        name: "Angular",
+        fact: "Angular is a development platform for building mobile and desktop web applications using TypeScript/JavaScript and other languages.",
+    }, {
+        name: "Ionic",
+        fact: "Ionic is the open-source mobile app development framework that makes it easy to build top quality native and progressive web apps with web technologies",
+    }, {
+        name: "Incubator Echarts",
+        fact: "ECharts is a free, powerful charting and visualization library offering an easy way of adding intuitive, interactive, and highly customizable charts to your commercial products.",
+    }, {
+        name: "Axios",
+        fact: "Axios is a Promise-based HTTP client for JavaScript which can be used in your front-end application and in your Node.js backend",
+    }, {
+        name: "Material2",
+        fact: "This is the home for the Angular team's UI components built for and with Angular. ",
+    }, {
+        name: "Express",
+        fact: " It is designed for building web applications and APIs. It has been called the de facto standard server framework for Node.js",
+    }, {
+        name: "Mocha",
+        fact: "Mocha is a JavaScript test framework for Node.js programs, featuring browser support, asynchronous testing, test coverage reports, and use of any assertion library.",
+    }, {
+        name: "Anime",
+        fact: "Anime.js is a lightweight JavaScript animation library with a simple,yet powerful API",
+    }, {
+        name: "Mobservable",
+        fact: "Mobservable enables your data structures to become observable.",
+    }, {
+        name: "Aframe",
+        fact: "Its a web framework for building virtual reality experiences.",
+    }, {
+        name: "Faker.js",
+        fact: "generate massive amounts of fake data in Node.js and the browser.",
+    }, {
+        name: "Backbone",
+        fact: "Backbone.js is a JavaScript library with a RESTful JSON interface and is based on the Model–view–presenter application design paradigm.",
+    }, {
+        name: "Bluebird",
+        fact: "The strongest feature of Bluebird is that it allows you to promisify other Node modules in order to use them asynchronously.",
+    }, {
+        name: "Handsontable",
+        fact: "It provides easy data binding, data validation, filtering, sorting and CRUD operations.",
+    }, {
+        name: "Chosen",
+        fact: "Chosen is a library for making long, unwieldy select boxes more friendly",
+    }, {
+        name: "Cheerio",
+        fact: "Fast, flexible, and lean implementation of core jQuery designed specifically for the server.",
+    }, {
+        name: "Bootstrap Table",
+        fact: "An extended Bootstrap table with radio, checkbox, sort, pagination, extensions and other added features.",
+    }, {
+        name: "Coffee Script",
+        fact: "CoffeeScript is a programming language that compiles to JavaScript.",
+    }, {
+        name: "Babylon.js",
+        fact: "Babylon.js: a complete JavaScript framework for building 3D games with HTML 5 and WebGL",
+    }, {
+        name: "Hammer.js",
+        fact: "Its a javascript library for multi-touch gestures ",
+    }, {
+        name: "FileSaver.js",
+        fact: "FileSaver.js implements the saveAs() FileSaver interface in browsers that do not natively support it.",
+    }, {
+        name: "c3",
+        fact: "c3 is a D3-based reusable chart library that enables deeper integration of charts into web applications",
+    }, {
+        name: "Bower",
+        fact: "Bower offers a generic, unopinionated solution to the problem of front-end package management, while exposing the package dependency model via an API that can be consumed by a more opinionated build stack.",
+    }, {
+        name: "Inferno",
+        fact: "The main objective of the InfernoJS project is to provide the fastest possible runtime performance for web applications. ",
+    }, {
+        name: "Local Forage",
+        fact: "localForage is a fast and simple storage library for JavaScript.",
+    }, {
+        name: "Chartist Js",
+        fact: "Chartist.js is a simple responsive charting library built with SVG.",
+    }, {
+        name: "Dragula",
+        fact: "Dragula provides the easiest possible API to make cross-browser (IE7 included!) drag-and-drop a breeze in your applications",
+    }, {
+        name: "Hyperapp",
+        fact: "The tiny framework for building web interfaces.",
+    }
+]
