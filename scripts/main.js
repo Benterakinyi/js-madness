017e7f17e4f62ddd54529700c546ec3f7bd27bd1
mutembeijoe <mutembeijoe@gmail.com>
2019-09-04 11:38:21 +0300

    add confete effect that is displayed when a user sucessfully guesses a lib

--- scripts/main.js
+++ scripts/main.js
@@ -59,8 +59,13 @@ function findLibraryMatch(answer){
 }
 function determineCorrectness(lib){
     if(lib != undefined){
-        if(lib.name.startsWith(letter)){
-            console.log("correct")
+        if(lib.name.toLowerCase().startsWith(letter)){
+            $('#timer').html(`
+                <span>&#10004;</span>
+                <p> Correct</p>
+            `)
+            $('body').prepend(`<div class="wrapper"></div>`);
+            displayConfete()
         }else{
             console.log('enter a lib that begins with the letter you were provided')
         }
@@ -68,10 +73,65 @@ function determineCorrectness(lib){
         console.log('failed')
     }
 }
+
+function displayConfete(){
+    for (var i = 0; i < 250; i++) {
+        create(i);
+    }
+      
+    function create(i) {
+        var width = Math.random() * 40;
+        var height = width * 0.4;
+        var colourIdx = Math.ceil(Math.random() * 3);
+        var colour = "red";
+        switch(colourIdx) {
+        case 1:
+            colour = "yellow";
+            break;
+        case 2:
+            colour = "blue";
+            break;
+        default:
+            colour = "red";
+        }
+        $('<div class="confetti-'+i+' '+colour+'"></div>').css({
+          "width" : width+"px",
+          "height" : height+"px",
+          "top" : -Math.random()*20+"%",
+          "left" : Math.random()*100+"%",
+          "opacity" : Math.random()+0.5,
+          "transform" : "rotate("+Math.random()*360+"deg)"
+        }).appendTo('.wrapper');  
+        
+        drop(i);
+    }
+      
+    function drop(x) {
+        $('.confetti-'+x).animate({
+            top: "100%",
+            left: "+="+Math.random()*15+"%"
+        }, Math.random()*3000 + 3000, function() {
+            reset(x);
+        });
+    }
+      
+    function reset(x) {
+        $('.confetti-'+x).animate({
+            "top" : -Math.random()*20+"%",
+            "left" : "-="+Math.random()*15+"%"
+        }, 0, function() {
+            drop(x);             
+        });
+    }
+}
+
+
+
 //  UI LOGIC
 $(document).ready(function(){
 
     $('#playButton').click(function(){
+        // $('.wrapper').remove();
         startGame();
         $(this).attr('disabled','true')
     })
@@ -79,7 +139,7 @@ $(document).ready(function(){
     $('#answerbutton').click(function(){
         const answer = $('#answerinput').val().toLowerCase();
         console.log(answer);
-        const foundLib = findLibraryMatch(answer)
+        const foundLib = findLibraryMatch(answer);
         determineCorrectness(foundLib);
     })
 
